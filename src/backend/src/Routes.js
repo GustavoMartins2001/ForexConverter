b36ac692430f2f6e903b4cad615a9d66befe1959
Gustavo <gustavo.santos3@tcs.com>
2025-06-26 12:29:29 -0300

    adicionado anotações para referencia futura

--- src/backend/src/Routes.js
+++ src/backend/src/Routes.js
@@ -4,15 +4,15 @@ const router = express.Router();
 router.get('/', (req, res) => {
     // caso o usuario não esteja logado, redireciona para a página de login
     //TODO: implementar session handler
-    res.send('Welcome to the Forex Converter API! Please log in to access the dashboard.');
-    // if(localStorage.getItem('user') === null) {
-    //     return router.route('/login').get((req, res) => {
-    //         res.send('Please log in to access the Forex Converter API');
-    //     })
-    // }
-    // else {
-    //     res.send('dashboardPageInfo');
-    // }
+    // res.send('Welcome to the Forex Converter API! Please log in to access the dashboard.');
+    if(localStorage.getItem('user') === null) {
+        return router.route('/login').get((req, res) => {
+            res.send('Please log in to access the Forex Converter API');
+        })
+    }
+    else {
+        res.send('dashboardPageInfo');
+    }
 });
 
 // Example route: Convert currency
